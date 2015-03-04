e700f475aaf2124e378e17383312882546677068
Ankit Kumar <ankitk@stanford.edu>
2015-03-03 20:34:47 -0800

    pushing lasagne

--- convert.py
+++ convert.py
@@ -1,15 +1,25 @@
 import theano
 import numpy as np
 import theano.tensor as T
+import extra_layers
 try:
-	import cc_layers
+	from lasagne.layers import cuda_convnet
 	cuda = True
 except:
-	print "probably no pylearn2; using normal"
+	print "probably no GPU, or no pylearn2 capabilities; using normal"
 	cuda = False
-import layers
+import lasagne.layers as layers
 from parse_model_def import parse_model_def as parse
 import caffe
+import lasagne.nonlinearities as nonlinearities
+
+# valid names (lowercased)
+valid_conv = set(['convolution'])
+valid_ip = set(['innerproduct', 'inner_product'])
+valid_relu = set(['relu'])
+valid_lrn = set(['lrn'])
+valid_pooling = set(['pooling'])
+valid_softmax = set(['softmax'])
 
 def convert(prototxt, caffemodel):
 	'''
@@ -24,25 +34,31 @@ def convert(prototxt, caffemodel):
 	net = caffe.Net(prototxt, caffemodel, caffe.TEST)
 
 	# create input layer
-	# this actually ends up being c01b shaped, but we pass in bc01
+	# this actually ends up being c01b shaped if cuda, but we pass in bc01
 	if cuda == False:
-		last_layer = inp_layer = layers.Input2DLayer(input_dims[0], input_dims[1], input_dims[2], input_dims[3])
+		last_layer = inp_layer = layers.InputLayer(tuple(input_dims), name='data')
 	else:
-		last_layer = inp_layer = cc_layers.CudaConvnetInput2DLayer(1, input_dims[1], input_dims[2], input_dims[3])
+		# this actually ends up being c01b shaped if cuda, but we pass in bc01, so we need to reshuffled
+		last_layer = inp_layer = layers.InputLayer((input_dims[1], input_dims[2], input_dims[3], input_dims[0]), name='data')
+
 
 	# go thru layers and create the theano layer 
 	all_layers = [inp_layer]
 	swapped = False
-	print architecture[0]
 	for layer in architecture:
 		if (layer['type'] == 'INNER_PRODUCT' or layer['type'] == 'InnerProduct') and swapped==False and cuda==True:
 			# need to add a reshaping layer
-			reshape_layer = cc_layers.ShuffleC01BToBC01Layer(last_layer)
+			'''
+			this might not be needed, check lasagne stuff
+			'''
+			reshape_layer = cuda_convnet.ShuffleC01BToBC01Layer(last_layer)
 			all_layers.append(reshape_layer)
 			last_layer = reshape_layer
 			swapped = True
+
 		this_layer = parse_layer(layer, last_layer)
 		if this_layer == -1:
+			# error checking
 			continue
 		
 		set_params(this_layer, net, layer)
@@ -137,51 +153,67 @@ def parse_layer(layer, last_layer):
 		return -1
 
 def lrn_layer_from_params(layer, last_layer):
-	lrn = layers.ResponseNormalisationLayer(last_layer, int(layer['local_size']), 1, float(layer['alpha']), float(layer['beta']))
-	print lrn
+	# set params
+	alpha = float(layer['alpha'])
+	beta = float(layer['beta'])
+	n = int(layer['local_size'])
+
+	lrn = extra_layers.CaffeLocalResponseNormalization2DLayer(last_layer, alpha=alpha, beta=beta, n=n)
 	return lrn
+
 def cuda_conv_layer_from_params(layer, last_layer):
-	''' CAN'T DO ANYTHING BUT (1,1) STRIDES RIGHT NOW! '''
-	if layer['stride'] == 'DEFAULT':
-		layer['stride'] = 1
-	if int(layer['kernel_size']) - (int(layer['pad']) * 2 ) == 1:
-		print "using same convolutions, this should be correct"
-	else:
-		print "this will be incorrect. the caffe net is not using same convolutions (i think). you should check what their doing, go into layers.py and fix this accordingly"
-		
-	conv = cc_layers.CudaConvnetConv2DLayer(last_layer, int(layer['num_output']), int(layer['kernel_size']), -1, -1, nonlinearity=layers.identity, pad=int(layer['pad']),stride=int(layer['stride']))
+	num_filters = int(layer['num_output'])
+	filter_size = int(layer['kernel_size'])
+	strides = (int(layer['stride']),int(layer['stride'])) # can only suport square strides anyways
+	## border mode is wierd...
+	border_mode = None
+	pad = int(layer['pad'])
+	nonlinearity=nonlinearities.identity
+			
+	conv = cuda_convnet.Conv2DCCLayer(last_layer, num_filters=num_filters,filter_size=filter_size, strides=strides, border_mode=border_mode, pad=pad, nonlinearity=nonlinearity)
 	return conv
 
 def conv_layer_from_params(layer, last_layer):
-	''' CAN'T DO ANYTHING BUT (1,1) STRIDES RIGHT NOW! '''
-	if layer['stride'] == 'DEFAULT':
-		layer['stride'] = 1
+	# theano's conv only allows for certain padding, not arbitrary. not sure how it will work if same border mode is not true.
 	if int(layer['kernel_size']) - (int(layer['pad']) * 2 ) == 1:
 		print "using same convolutions, this should be correct"
 	else:
 		print "this will be incorrect. the caffe net is not using same convolutions (i think). you should check what their doing, go into layers.py and fix this accordingly"
 		raise Exception ("this will probably not work but try to comment this out if oyu want")
-	conv = layers.Conv2DLayer(last_layer, int(layer['num_output']), int(layer['kernel_size']), int(layer['kernel_size']), -1, -1, nonlinearity=layers.identity, border_mode='same')
+
+	num_filters = int(layer['num_output'])
+	filter_size = int(layer['kernel_size'])
+	strides = (int(layer['stride']),int(layer['stride'])) # can only suport square strides anyways
+	## border mode is wierd...
+	border_mode = same
+	nonlinearity=nonlinearities.identity
+
+	conv = layers.Conv2DLayer(last_layer, num_filters=num_filters, filter_size=filter_size, strides=strides, border_mode='same', linearity=nonlinearity)
 	return conv
 
 def relu_layer_from_params(layer, last_layer):
-	return layers.ReluLayer(last_layer)
+	return extra_layers.ReluLayer(last_layer)
 
 def pooling_layer_from_params(layer, last_layer):
-	if layer['stride'] == 'DEFAULT':
-		layer['stride'] = 1
-	pool = layers.Pooling2DLayer(last_layer, (int(layer['kernel_size']), int(layer['kernel_size'])))
+	ds=(int(layer['kernel_size']),int(layer['kernel_size'])) #caffe only does square kernels
+
+	pool = layers.MaxPool2DLayer(last_layer, ds=ds) # ignore border is set to False, maybe look into how caffe does borders if the strides don't work perfectly
 	return pool
 
 def cuda_pooling_layer_from_params(layer, last_layer):
-	if layer['stride'] == 'DEFAULT':
-		layer['stride'] = 1
-		print "this is a problem."
-	pool = cc_layers.CudaConvnetPooling2DLayer(last_layer, pool_size=int(layer['kernel_size']), stride=int(layer['stride']))
+	ds = (int(layer['kernel_size']),int(layer['kernel_size'])) # cuda only supports square anyways
+	stride = (int(layer['stride']), int(layer['stride'])) #only square strides as well
+
+	pool = cuda_convnet.MaxPool2DCCLayer(last_layer, ds=ds, strides=strides)
 	return pool
 
 def ip_layer_from_params(layer, last_layer):
-	dense = layers.DenseCaffeLayer(last_layer, int(layer['num_output']), -1, -1, nonlinearity=layers.identity) #dropout default 0
+	num_units=int(layer['num_output'])
+	nonlinearity=layers.identity
+	if cuda:
+		dense = extra_layers.CaffeDenseLayer(last_layer, num_units=num_units, nonlinearity=nonlinearity)
+	else:
+		dense = layers.DenseLayer(last_layer, num_units=num_units, nonlinearity=nonlinearity)
 	return dense
 
 def dropout_layer_from_params(layer, last_layer):
@@ -189,10 +221,10 @@ def dropout_layer_from_params(layer, last_layer):
 	TODO: IMPLEMENT THIS. currently only using this script for forward passes, so this can be a complete identity
 	but in the future maybe i'll want to finetune, so this would need to be implemented.
 	'''
-	return layers.IdentityLayer(last_layer)
+	return extra_layers.IdentityLayer(last_layer)
 
 def softmax_layer_from_params(layer, last_layer):
-	return layers.SoftmaxLayer(last_layer)
+	return extra_layers.SoftmaxLayer(last_layer)
 
 if __name__ == '__main__':
 	forward, net, all_layers = convert('/root/caffe/models/bvlc_reference_rcnn_ilsvrc13/deploy.prototxt','/root/caffe/models/bvlc_reference_rcnn_ilsvrc13/bvlc_reference_rcnn_ilsvrc13.caffemodel')
--- model.py
+++ model.py
@@ -1,16 +1,43 @@
-import layers
-import cc_layers
+import lasagne
+import lasagne.layers as ll
 import theano
 import theano.tensor as T
 
-class conv_model(object):
-	def __init__(self, layers):
-		self.layers = layers # this should be a list of layers, 
-		self.forward, self.backward = self.compile_functions()
+
+class LasagneModel(object):
+	def __init__(self, last_layer):
+		# get all the layers
+		self.all_layers = ll.get_all_layers(last_layer)
+		# save input, last layer
+		# currently assumed that all_layers[0] will be input
+		self.last_layer = last_layer
+		self.input_layer = self.all_layers[0]
 
 
-	def compile_functions(self):
+	def compile_forward(self, nOutputs=1):
 		'''
-		compiles the forward and backward pass of this conv model
+		outputs is the number of outputs to return (starting from the last layer)
+
+		this should be extended to take a list of layer names instead (or in addition)
+
+		TODO: not sure why people always make the input to the theano function an index rather than index the dataset and just input that, i.e:
+		f=theano.function([idx], output, givens={self.input_layer.input_var:ds[idx]})
+		for idx in __:
+			f(idx)
+		vs
+		f = theano.function([self.input_layer.input_var],output)
+		for idx in __:
+			f(ds[idx])
+
+		maybe do some benchmarking and see which is faster (could be how shared datasets are used), but for now this is cleaner
 		'''
-		inp = T
+		# get symbolic input from layer
+		symbolic_input = self.input_layer.input_var
+		# make list of outputs
+		outputs = [layer.get_output() for layer in self.all_layers[-nOutputs:]]
+		# store function in self.forward
+		self.forward = theano.function([symbolic_input], outputs)
+
+
+
+
