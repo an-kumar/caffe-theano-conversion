12619438d31147f8870abd7b8965d0b5a502c403
Ankit Kumar <ankitk@stanford.edu>
2015-03-04 02:15:45 -0800

    now im just making dumb mistakes..

--- extra_layers.py
+++ extra_layers.py
@@ -17,7 +17,7 @@ import lasagne.init as init
 
 class CaffeConv2DCCLayer(cuda_convnet.Conv2DCCLayer):
     def __init__(self, incoming, num_filters, filter_size, groups=1, strides=(1, 1), border_mode=None, untie_biases=False, W=init.Uniform(), b=init.Constant(0.), nonlinearity=nonlinearities.rectify, pad=None, dimshuffle=True, flip_filters=False, partial_sum=1, **kwargs):
-        super(CaffeConv2DCCLayer).__init__(incoming, num_filters, filter_size, strides=strides, border_mode=border_mode, untie_biases=untie_biases, W=W, b=b, nonlinearity=nonlinearity, pad=pad, dimshuffle=dimshuffle, flip_filters=flip_filters, partial_sum=partial_sum, **kwargs)
+        super(CaffeConv2DCCLayer,self).__init__(incoming, num_filters, filter_size, strides=strides, border_mode=border_mode, untie_biases=untie_biases, W=W, b=b, nonlinearity=nonlinearity, pad=pad, dimshuffle=dimshuffle, flip_filters=flip_filters, partial_sum=partial_sum, **kwargs)
         self.groups = groups
         self.filter_acts_op = FilterActs(numGroups=self.groups, stride=self.stride, partial_sum=self.partial_sum, pad=self.pad)
 
