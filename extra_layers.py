c1869a6468ee6125dadf9effb08c948ec67ba854
Ankit Kumar <ankitk@stanford.edu>
2015-03-04 02:16:45 -0800

    import pylearn2

--- extra_layers.py
+++ extra_layers.py
@@ -14,6 +14,10 @@ import lasagne.layers as layers
 import lasagne.nonlinearities as nonlinearities
 import lasagne.init as init
 
+from theano.sandbox.cuda.basic_ops import gpu_contiguous
+# need to do my change.
+from pylearn2.sandbox.cuda_convnet.filter_acts import FilterActs
+
 
 class CaffeConv2DCCLayer(cuda_convnet.Conv2DCCLayer):
     def __init__(self, incoming, num_filters, filter_size, groups=1, strides=(1, 1), border_mode=None, untie_biases=False, W=init.Uniform(), b=init.Constant(0.), nonlinearity=nonlinearities.rectify, pad=None, dimshuffle=True, flip_filters=False, partial_sum=1, **kwargs):
