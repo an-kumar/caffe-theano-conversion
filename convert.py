6e5d998105abc28e493bff2532fdcc9e31f4dab2
Ankit Kumar <ankitk@stanford.edu>
2015-03-07 18:29:17 -0800

    moving to terminal

--- convert.py
+++ convert.py
@@ -15,28 +15,268 @@ except Exception as e:
 	print "probably no GPU, or no pylearn2 capabilities; using normal"
 	cuda = False
 import lasagne.layers as layers
-from parse_model_def import parse_model_def as parse
-import caffe
+# from parse_model_def import parse_model_def as parse
+from parse_from_protobuf import parse_model_def, parse_caffemodel
+try:
+	import caffe
+except:
+	print 'continuing without caffe'
 import lasagne.nonlinearities as nonlinearities
 
+V1Map = {0: 'NONE',
+ 1: 'ACCURACY',
+ 2: 'BNLL',
+ 3: 'CONCAT',
+ 4: 'CONVOLUTION',
+ 5: 'DATA',
+ 6: 'DROPOUT',
+ 7: 'EUCLIDEAN_LOSS',
+ 8: 'FLATTEN',
+ 9: 'HDF5_DATA',
+ 10: 'HDF5_OUTPUT',
+ 11: 'IM2COL',
+ 12: 'IMAGE_DATA',
+ 13: 'INFOGAIN_LOSS',
+ 14: 'INNER_PRODUCT',
+ 15: 'LRN',
+ 16: 'MULTINOMIAL_LOGISTIC_LOSS',
+ 17: 'POOLING',
+ 18: 'RELU',
+ 19: 'SIGMOID',
+ 20: 'SOFTMAX',
+ 21: 'SOFTMAX_LOSS',
+ 22: 'SPLIT',
+ 23: 'TANH',
+ 24: 'WINDOW_DATA',
+ 25: 'ELTWISE',
+ 26: 'POWER',
+ 27: 'SIGMOID_CROSS_ENTROPY_LOSS',
+ 28: 'HINGE_LOSS',
+ 29: 'MEMORY_DATA',
+ 30: 'ARGMAX',
+ 31: 'THRESHOLD',
+ 32: 'DUMMY_DATA',
+ 33: 'SLICE',
+ 34: 'MVN',
+ 35: 'ABSVAL',
+ 36: 'SILENCE',
+ 37: 'CONTRASTIVE_LOSS',
+ 38: 'EXP',
+ 39: 'DECONVOLUTION'}
 
 # valid names (lowercased)
-valid_conv = set(['convolution'])
-valid_ip = set(['innerproduct', 'inner_product'])
+valid_convolution = set(['convolution'])
+valid_inner_product = set(['innerproduct', 'inner_product'])
 valid_relu = set(['relu'])
 valid_lrn = set(['lrn'])
 valid_pooling = set(['pooling'])
 valid_softmax = set(['softmax'])
+valid_dropout = set(['dropout'])
+
+
+
+def convert_model_def(prototxt):
+	'''
+	prototxt is a model definition .prototxt file
+
+	returns a lasagne model with the layers, correct initialized
+	'''
+	name, inp, architecture = parse_model_def(prototxt)
+	inp_name, inp_dims = inp
+
+	last_layer = inp_layer = layers.InputLayer(tuple(inp_dims), name=inp_name)
+	# now go through all layers and create the lasagne equivalent
+	for layer in architecture:
+		this_layer = parse_layer_from_param(layer, last_layer)
+		last_layer = this_layer
+
+	# make the lasagne model (only need last layer)
+	model = LasagneModel(last_layer)
+	return model
+
+def parse_layer_from_param(layer,last_layer):
+	'''
+	returns the correct layer given the param dict
+	'''
+	tp = V1Map[layer.type].lower()
+
+	if tp in valid_convolution:
+		if cuda==True:
+			return cuda_conv_layer(layer, last_layer)
+		else:
+			return conv_layer(layer, last_layer)
+	elif tp in valid_relu:
+		return relu_layer(layer, last_layer)
+	elif tp in valid_pooling:
+		if cuda==True:
+			return cuda_pooling_layer(layer, last_layer)
+		else:
+			return pooling_layer(layer, last_layer)
+	elif tp in valid_inner_product:
+		return ip_layer(layer, last_layer)
+	elif tp in valid_dropout:
+		return dropout_layer(layer, last_layer)
+	elif tp in valid_softmax:
+		return softmax_layer(layer, last_layer)
+	elif tp in valid_lrn:
+		return lrn_layer(layer, last_layer)
+	else:
+		raise Exception('not a valid layer: %s' % tp)
+
+
+
+def lrn_layer(layer, last_layer):
+	name = layer.name
+	param = layer.lrn_param
+	# set params
+	alpha = param.alpha
+	beta = param.beta
+	n = param.local_size
+
+	lrn = extra_layers.CaffeLocalResponseNormalization2DLayer(last_layer, alpha=alpha, beta=beta, n=n,name=name)
+	return lrn
+
+def cuda_conv_layer(layer, last_layer):
+	name = layer.name
+	param = layer.convolution_param
+
+	num_filters = param.num_output
+	filter_size = (param.kernel_size,param.kernel_size) #only suppose square filters
+	strides = (param.stride,param.stride) # can only suport square strides anyways
+	## border mode is wierd...
+	border_mode = None
+	pad = param.pad
+	nonlinearity=nonlinearities.identity
+	groups= param.group
+			
+	conv = extra_convnet_layers.CaffeConv2DCCLayer(last_layer, groups=groups, num_filters=num_filters,filter_size=filter_size, strides=strides, border_mode=border_mode, pad=pad, nonlinearity=nonlinearity,name=name)
+	return conv
+
+def conv_layer(layer, last_layer):
+	name = layer.name
+	param = layer.convolution_param
+	num_filters = param.num_output
+	filter_size = (param.kernel_size, param.kernel_size)
+	strides = (param.stride, param.stride)
+	group = param.group
+	pad = param.pad
+	nonlinearity=nonlinearities.identity
+
+	# theano's conv only allows for certain padding, not arbitrary. not sure how it will work if same border mode is not true.
+	if (filter_size[0] - pad * 2 ) == 1:
+		print "using same convolutions, this should be correct"
+		border_mode = 'same'
+	elif pad == 0:
+		print "using valid border mode, this should work but who knows"
+		border_mode='valid'
+	elif pad != 0:
+		print "pretty sure this won't work but we'll try a full conv"
+		border_mode = 'full'
+	if group > 1:
+		conv = extra_convnet_layers.CaffeConv2DLayer(last_layer, group=group,num_filters=num_filters, filter_size=filter_size, strides=strides, border_mode=border_mode, nonlinearity=nonlinearity,name=name)
+	else:
+		conv = layers.Conv2DLayer(last_layer, num_filters=num_filters, filter_size=filter_size, strides=strides, border_mode=border_mode, nonlinearity=nonlinearity,name=name)
+	return conv
+
+def relu_layer(layer, last_layer):
+	name = layer.name
+	return extra_layers.ReluLayer(last_layer,name=name)
+
+def pooling_layer(layer, last_layer):
+	name = layer.name
+	param = layer.pooling_param
+	ds=(param.kernel_size,param.kernel_size) #caffe only does square kernels
+	strides = (param.stride, param.stride)
+
+	if strides[0] != ds[0]:
+		pool = extra_layers.CaffeMaxPool2DLayer(last_layer,ds=ds, strides=strides,name=name)
+	else:
+		pool = layers.MaxPool2DLayer(last_layer, ds=ds,name=name) # ignore border is set to False, maybe look into how caffe does borders if the strides don't work perfectly
+	return pool
+
+def cuda_pooling_layer(layer, last_layer):
+	name = layer.name
+	param = layer.pooling_param
+	ds=(param.kernel_size,param.kernel_size) #caffe only does square kernels
+	strides = (param.stride, param.stride)
+
+	pool = cuda_convnet.MaxPool2DCCLayer(last_layer, ds=ds, strides=strides,name=name)
+	return pool
+
+def ip_layer(layer, last_layer):
+	name = layer.name
+	param = layer.inner_product_param
+	num_units=param.num_output
+	nonlinearity=nonlinearities.identity
+
+	dense = layers.DenseLayer(last_layer, num_units=num_units, nonlinearity=nonlinearity,name=name)
+	return dense
+
+def dropout_layer(layer, last_layer):
+	name = layer.name
+	'''
+	TODO: IMPLEMENT THIS. currently only using this script for forward passes, so this can be a complete identity
+	but in the future maybe i'll want to finetune, so this would need to be implemented.
+	'''
+	return extra_layers.IdentityLayer(last_layer, name=name)
+
+def softmax_layer(layer, last_layer):
+	name = layer.name
+	return extra_layers.SoftmaxLayer(last_layer, name=name)
+
+
+
+
+
+def set_params_from_caffemodel(lasagne_model, caffemodel):
+	'''
+	sets the params of lasagne_model to be from the trained caffemodel.
+
+	lasagne_model is a lasagne model from e.g convert_model_def(prototxt).
+	caffemodel is the filepath to the .caffemodel file
+	'''
+	# load in the caffemodel (this takes a long time without cpp implementation of protobuf)
+	layer_params = parse_caffemodel(caffemodel)
+
+	# this should be in the same order as was made by the lasagne model, but reversed. we will check that.
+	# todo: maybe just go by names, strictly? 
+	for layer in layer_params[::-1]:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def convert(prototxt, caffemodel):
 	'''
 	prototxt is a .prototxt file
 	caffemodel is a .caffemodel file
-
-	returns a function "forward", that's all. of course this code can be changed to do a lot of things.
 	'''
 	# parse the prototxt file
 	input_dims, architecture = parse(prototxt)
+	input_dims2, architecture2 = parse2(prototxt)
+	return input_dims, architecture, input_dims2, architecture2
 	assert len(input_dims) == 4 #bc01
 	net = caffe.Net(prototxt, caffemodel, caffe.TEST)
 
@@ -297,7 +537,14 @@ if __name__ == '__main__':
 	parser.add_argument("--caffemodel", default='VGG_ILSVRC_16_layers.caffemodel',help="model binary")
 	args = parser.parse_args()
 
+	model = convert_model_def(args.prototxt)
+	raise
+
 	print 'Converting model...'
+	one,two,a,b = convert(args.prototxt,args.caffemodel)
+	raise
+
+
 	model, net, all_layers = convert(args.prototxt,args.caffemodel)
 	print 'testing similarity...'
 	random_mat, outlist =test_similarity(model, net)
---
+++ parse_from_protobuf.py
@@ -0,0 +1,248 @@
+from google.protobuf import text_format
+import caffe_pb2
+
+
+valid_convolution = set(['convolution'])
+valid_inner_product = set(['innerproduct', 'inner_product'])
+valid_relu = set(['relu'])
+valid_lrn = set(['lrn'])
+valid_pooling = set(['pooling'])
+valid_softmax = set(['softmax'])
+valid_dropout = set(['dropout'])
+
+V1Map = {0: 'NONE',
+ 1: 'ACCURACY',
+ 2: 'BNLL',
+ 3: 'CONCAT',
+ 4: 'CONVOLUTION',
+ 5: 'DATA',
+ 6: 'DROPOUT',
+ 7: 'EUCLIDEAN_LOSS',
+ 8: 'FLATTEN',
+ 9: 'HDF5_DATA',
+ 10: 'HDF5_OUTPUT',
+ 11: 'IM2COL',
+ 12: 'IMAGE_DATA',
+ 13: 'INFOGAIN_LOSS',
+ 14: 'INNER_PRODUCT',
+ 15: 'LRN',
+ 16: 'MULTINOMIAL_LOGISTIC_LOSS',
+ 17: 'POOLING',
+ 18: 'RELU',
+ 19: 'SIGMOID',
+ 20: 'SOFTMAX',
+ 21: 'SOFTMAX_LOSS',
+ 22: 'SPLIT',
+ 23: 'TANH',
+ 24: 'WINDOW_DATA',
+ 25: 'ELTWISE',
+ 26: 'POWER',
+ 27: 'SIGMOID_CROSS_ENTROPY_LOSS',
+ 28: 'HINGE_LOSS',
+ 29: 'MEMORY_DATA',
+ 30: 'ARGMAX',
+ 31: 'THRESHOLD',
+ 32: 'DUMMY_DATA',
+ 33: 'SLICE',
+ 34: 'MVN',
+ 35: 'ABSVAL',
+ 36: 'SILENCE',
+ 37: 'CONTRASTIVE_LOSS',
+ 38: 'EXP',
+ 39: 'DECONVOLUTION'}
+
+def parse_model_def(filepath):
+	'''
+	parses the model definition .prototxt file.
+
+	filepath: /path/to/model-def.prototxt
+
+	returns: (input_dimensions, list_of_layers)
+	'''
+	# open the file and read it's contents
+	f = open(filepath)
+	contents = f.read()
+	f.close()
+
+	# make a netparameter object
+	netparam = caffe_pb2.NetParameter()
+	# read the contents into the net parameters
+	text_format.Merge(contents, netparam)
+
+	# now go through and turn the netparam into a list of dictionaries storing layers
+	# this is done to standardize across ".layer", ".layers", ".input_shape", ".input_dim", etc.
+	input_dims = find_input_dims(netparam)
+	# now we get all layers
+	layers = find_layers(netparam)
+
+	name = netparam.name
+	inp = (netparam.input[0], input_dims)
+
+	return name, inp, layers
+
+
+
+def parse_caffemodel(filepath):
+	'''
+	parses the trained .caffemodel file
+
+	filepath: /path/to/trained-model.caffemodel
+
+	returns: the layerparameter objects, in order
+	'''
+	f = open(filepath)
+	contents = f.read()
+
+	netparam = caffe_pb2.NetParameter()
+	netparam.ParseFromString(contents)
+
+	return find_layers(netparam)
+
+
+
+
+
+
+
+def parse_architecture_prototxt(filepath):
+	'''
+	parses the model definition .prototxt file.
+
+	filepath: /path/to/model-def.prototxt
+
+	returns: (input_dimensions, list_of_layers)
+	'''
+	# open the file and read it's contents
+	f = open(filepath)
+	contents = f.read()
+
+	# make a netparameter object
+	netparam = caffe_pb2.NetParameter()
+	# read the contents into the net parameters
+	text_format.Merge(contents, netparam)
+
+	# now go through and turn the netparam into a list of dictionaries storing layers
+	# this is done to standardize across ".layer", ".layers", ".input_shape", ".input_dim", etc.
+	input_dims = find_input_dims(netparam)
+	# now we get all layers
+	print "parsing layers..."
+	layers = []
+	for layer in find_layers(netparam):
+		layers.append(layer_dict_from_layer(layer))
+	
+	# and return
+	return input_dims, layers
+
+
+def parse_model_def(filepath):
+	'''
+	parses the model definition .prototxt file.
+
+	filepath: /path/to/model-def.prototxt
+
+	returns: (input_dimensions, list_of_layers)
+	'''
+	# open the file and read it's contents
+	f = open(filepath)
+	contents = f.read()
+
+	# make a netparameter object
+	netparam = caffe_pb2.NetParameter()
+	# read the contents into the net parameters
+	text_format.Merge(contents, netparam)
+
+	# now go through and turn the netparam into a list of dictionaries storing layers
+	# this is done to standardize across ".layer", ".layers", ".input_shape", ".input_dim", etc.
+	input_dims = find_input_dims(netparam)
+	# now we get all layers
+	layers = find_layers(netparam)
+
+	name = netparam.name
+	inp = (netparam.input[0], input_dims)
+	return name, inp, layers
+
+
+
+
+
+def layer_dict_from_layer(layer):
+	ldict = {}
+	ltype = find_layer_type(layer)
+	ldict['type'] = ltype
+	ldict['name'] = layer.name
+
+
+	if ltype in valid_convolution:
+		print "found conv"
+		ldict['num_output'] = layer.convolution_param.num_output
+		ldict['kernel_size'] = layer.convolution_param.kernel_size
+		ldict['weight_filler'] = layer.convolution_param.weight_filler
+		ldict['bias_term'] = layer.convolution_param.bias_term
+		ldict['stride'] = layer.convolution_param.stride
+		ldict['group'] = layer.convolution_param.group
+
+	elif ltype in valid_pooling:
+		print "found pool"
+		ldict['kernel_size'] = layer.pooling_param.kernel_size
+		ldict['pool'] = layer.pooling_param.pool
+		ldict['pad'] = layer.pooling_param.pad
+		ldict['stride'] = layer.pooling_param.stride
+
+	elif ltype in valid_lrn:
+		print "found lrn"
+		ldict['local_size'] = layer.lrn_param.local_size
+		ldict['alpha'] = layer.lrn_param.alpha
+		ldict['beta'] = layer.lrn_param.beta
+		ldict['norm_region'] = layer.lrn_param.norm_region
+
+	elif ltype in valid_inner_product:
+		print "found inner product"
+		ldict['num_output'] = layer.inner_product_param.num_output
+		ldict['weight_filler'] = layer.inner_product_param.weight_filler
+		ldict['bias_filler'] = layer.inner_product_param.bias_filler
+		ldict['bias_term'] = layer.inner_product_param.bias_term
+
+	elif ltype in valid_dropout:
+		print "found dropout"
+		ldict['dropout_ratio'] = layer.dropout_param.dropout_ratio
+
+	elif ltype in valid_relu:
+		print "found relu"
+		ldict['negative_slope'] = layer.relu_param.negative_slope
+
+	elif ltype in valid_softmax:
+		print "found softmax"
+	else:
+		raise Exception("That layer type (%s) is not currently supported" % ltype)
+
+	return ldict
+
+def find_layer_type(layer):
+	'''
+	checks if the netparam is V1 and if so maps the type
+
+	RETURNS IN LOWER CASE!
+	'''
+	if type(layer) == caffe_pb2.V1LayerParameter:
+		return V1Map[layer.type].lower()
+
+
+def find_layers(netparam):
+	if len(netparam.layers) > 0:
+		return netparam.layers
+	elif len(netparam.layer) > 0:
+		return netparam.layer
+	else:
+		raise Exception ("Couldn't find layers")
+
+
+def find_input_dims(netparam):
+	'''
+	checks netparam.input_dims and .input_shape to find which stores the shape
+	'''
+	if len(netparam.input_dim) > 0:
+		return netparam.input_dim
+	elif len(netparam.input_shape) > 0:
+		return netparam.input_shape
+	else:
+		raise Exception("Couldn't find input dimensions in the NetParameter object")
