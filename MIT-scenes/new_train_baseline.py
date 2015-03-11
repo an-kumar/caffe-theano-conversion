c6943aa72db98bb7ea19485f21a9d02c09f1b9e3
Ankit Kumar <ankitk@stanford.edu>
2015-03-10 23:15:40 -0700

    pushing new stuff

---
+++ MIT-scenes/new_train_baseline.py
@@ -0,0 +1,99 @@
+from solver import *
+from lasagne import layers, nonlinearities,objectives, updates, init, utils, regularization
+import theano
+import theano.tensor as T
+import sys
+sys.path.insert(0,'../')
+from caffe2theano import models
+import numpy as np
+
+'''
+SCORES: FC6 LogisticRegression: 0.56315789473684208
+		FC7 LogisticRegression: 0.55263157894736847
+
+		SIMPLE ENSEMBLE FC6,FC7: 0.58120300751879694
+
+		SIMPLE AVERAGE FC6, FC7: 0.5759398496240602
+
+		GATED AVERAGE: .599 !!
+
+'''
+class Eye(init.Initializer):
+	def __init__(self, scale=1):
+		self.scale=scale
+
+	def sample(self, shape):
+		assert shape[0] == shape[1]
+		assert len(shape) == 2
+		return utils.floatX(np.eye(shape[0]) * self.scale)
+
+class GatedMultipleInputsLayer(layers.MultipleInputsLayer):
+	'''
+	A layer that takes in multiple inputs *of the same dimensionality* and computes gates to combine them
+	'''
+	def __init__(self, incomings, Ws=init.Uniform(), bs = init.Constant(0.), nonlinearity=nonlinearities.sigmoid, **kwargs):
+		super(GatedMultipleInputsLayer,self).__init__(incomings,**kwargs)
+		num_out = self.input_shapes[0][1]
+		# make gates
+		self.Ws = [self.create_param(Ws, (num_out,num_out)) for i in range(len(incomings))]
+		self.bs = [self.create_param(bs, (num_out,)) for i in range(len(incomings))]
+
+		self.num_inputs = len(incomings)
+		self.nonlinearity = nonlinearity
+
+
+	def get_output_for(self, inputs, *args, **kwargs):
+		# compute gates
+		gs = [self.nonlinearity(T.dot(inputs[i], self.Ws[i]) + self.bs[i].dimshuffle('x',0)) for i in range(self.num_inputs)]
+		# hadamard product
+		new_inps = [gs[i] * inputs[i] for i in range(self.num_inputs)]
+		# stack into one tensor
+		tens = T.stack(*new_inps)
+		# now average
+		return T.mean(tens, axis=0)
+
+	def get_params(self):
+		return self.Ws + self.bs
+
+	def get_output_shape_for(self, input_shapes):
+		# assert that the input shapes are the same
+		assert len(set(input_shapes)) == 1
+		# output is the same as input
+		return input_shapes[0]
+
+	def get_output(self, inputs, *args, **kwargs):
+		'''
+		overwrite the get_output function, this probably could be changed in lasagne.
+		'''
+		layer_inputs = [self.input_layers[i].get_output(inputs[i], *args, **kwargs) for i in range(self.num_inputs)]
+		return self.get_output_for(layer_inputs,*args,**kwargs)
+
+LEARNING_RATE =0.008
+MOMENTUM=0.9
+REG = .01
+solv = SGDMomentumSolver(LEARNING_RATE)
+batch_size = 50
+# == defining model == # 
+input_one = layers.InputLayer((50, 4096))
+input_two = layers.InputLayer((50,4096))
+gated_avg = GatedMultipleInputsLayer([input_one,input_two])
+output = layers.DenseLayer(gated_avg, num_units=67, nonlinearity=nonlinearities.softmax)
+lmodel = models.BaseModel(output)
+
+
+# load datasets
+X_train_fc6 = np.load('/root/proj/MIT_dumped/X_train_fc6.npy')
+X_test_fc6 = np.load('/root/proj/MIT_dumped/X_test_fc6.npy')
+y_train = np.load('/root/proj/MIT_dumped/y_train.npy')
+y_test = np.load('/root/proj/MIT_dumped/y_test.npy')
+# load datasets
+X_train_fc7 = np.load('/root/proj/MIT_dumped/X_train_fc7.npy')
+X_test_fc7 = np.load('/root/proj/MIT_dumped/X_test_fc7.npy')
+# make pred func (todo: put in model)
+pred = T.argmax(
+    lmodel.get_output([X_batch_one, X_batch_two], deterministic=True), axis=1)
+lmodel.pred_func = T.mean(T.eq(pred, y_batch), dtype=theano.config.floatX)
+# make ds class
+ds = MultipleInputDataset([X_train_fc6,X_train_fc7], y_train, [X_test_fc6, X_test_fc7], y_test)
+num_epochs= 1000
+solver.solve(lmodel, ds, batch_size, num_epochs)
---
+++ MIT-scenes/solver.py
@@ -0,0 +1,207 @@
+import theano
+import theano.tensor as T
+import lasagne
+'''
+usage should be:
+solver.solve(model, dataset, batch_size)
+
+for now, dataset is a shared variable (later turned into object of a ds class)
+'''
+
+def tensortype_from_shape(shape, intg=False):
+	if len(shape) == 1:
+		''' HACK!:'''
+		if intg:
+			return T.ivector
+		else:
+			return T.vector
+	elif len(shape) == 2:
+		return T.matrix
+	elif len(shape) == 3:
+		return T.tensor3
+	elif len(shape) == 4:
+		return T.tensor4
+
+class MultipleInputDataset(object):
+	'''
+	for now, extremely simple. todo: decompose to BaseDataset
+	'''
+	def __init__(self, X_trains, y_train, X_tests, y_test, y_cast='int32'):
+		'''
+		all args are numpy format
+		'''
+		self.X_trains = [theano.shared(X_train.astype(theano.config.floatX)) for X_train in X_trains]
+		self.y_train = T.cast(theano.shared(y_train.astype(theano.config.floatX)),y_cast)
+		self.X_tests = [theano.shared(X_test.astype(theano.config.floatX)) for X_test in X_test]
+		self.y_test = T.cast(theano.shared(y_test.astype(theano.config.floatX)),y_cast)
+
+		self.X_batch_var = self.get_X_batch_var()
+		self.y_batch_var = self.get_y_batch_var()
+
+	def get_model_X_batch_var(self, X_trains):
+		return [tensortype_from_shape(X_train.shape)() for X_train in self.X_trains]
+	def get_model_y_batch_var(self, y_train):
+		return tensortype_from_shape(y_train.shape, intg=True)()
+
+	def train_givens(self, batch_index, batch_size):
+		'''
+		batch_index is a theano variable
+		'''
+		givens = {self.X_batch_var[i]:self.X_trains[i][batch_index*batch_size:(batch_index+1)*batch_size] for i in range(len(self.X_trains)) }
+		givens[self.y_batch_var] = self.y_train[batch_index*batch_size:(batch_index+1)*batch_size]
+		return givens
+
+
+
+
+class SGDMomentumSolver(BaseSolver):
+	'''
+	TODO: support changing momentum after #epochs
+	'''
+	def __init__(self, global_lr, momentum=0.9, lr_drop_epochs=None, gamma=0.9, objective='multinomial_nll',**kwargs):
+		'''
+		global_lr : global learning rate
+		momentum : momentum of SGD
+		lr_drop_epochs : num epochs before the lr drops
+		gamma : after lr_drop_epochs, the lrs become gamma*lr
+		'''
+		super(SGDMomentumSolver,self).__init__(**kwargs)
+		self.global_lr = global_lr
+		self.momentum = momentum
+		self.specific_W_lrs = {}
+		self.specific_b_lrs = {}
+		self.objective=objective
+
+	def set_specific_W_lrs(self, lr_dict):
+		'''
+		sets specific learning rates given layer **names**
+
+		lr_dict : dict mapping layer name -> specific learning rate
+		'''
+		# iterate through keys to overwrite already set keys
+		for key in lr_dict:
+			self.specific_W_lrs[key] = lr_dict[key]
+
+	def set_specific_b_lrs(self, lr_dict):
+		'''
+		sets specific learning rate given layer **names**
+
+		lr_dict : dict mapping layer name -> specific learning rate
+		'''
+		for key in lr_dict:
+			self.specific_b_lrs[key] = lr_dict[key]
+
+	def set_specific_lrs(self, lr_dict):
+		'''
+		wraps W and b
+		'''
+		self.set_specific_b_lrs(lr_dict)
+		self.set_specific_W_lrs(lr_dict)
+
+	def solve(self, model, dataset, batch_size, num_epochs, include_bias=True):
+		# get the obj_loss, reg_loss
+		X_batch = dataset.X_batch_var # could be a list
+		y_batch = dataset.y_batch_var # could be a list
+		obj = self.find_objective(model, self.objective)
+		obj_loss = obj.get_loss(X_batch, target=y_batch)
+
+		all_params = self.get_all_params(model, include_bias=include_bias)
+		reg_loss = self.get_reg_loss(model, include_bias=False)
+
+		updates, all_lrs = self.get_updates(obj_loss + reg_loss, all_params)
+
+		# init all lrs to the global lr
+		solver_givens = {val:self.global_lr for val in all_lrs.values()}
+		# go through and update the ones that have been set specifically
+		for name in self.specific_W_lrs:
+			# get the correct param
+			params = model.get_W_params_by_name(name)
+			for param in params:
+				solver_givens[all_lrs[param]] = self.specific_W_lrs[name]
+
+		for name in self.specific_b_lrs:
+			params = model.get_b_params_by_name(name)
+			for param in params:
+				solver_givens[all_lrs[param]] = self.specific_b_lrs[name]
+
+		# add dataset givens
+		# todo: is "batch_index" not general enough?
+		batch_index = T.iscalar()
+		solver_givens.update(dataset.train_givens(batch_index, batch_size))
+
+		# compile function
+		# todo: obj loss?
+		func = theano.function([batch_index], obj_loss, updates=updates, givens=solver_givens)
+		if model.pred_func is not None:
+			test_func = theano.function([], pred_func, givens=dataset.test_givens())
+		else:
+			test_func = None
+		# train
+		batches_per_epoch = (dataset.train_size / batch_size) + 1
+		loss_history = []
+		for epoch in range(num_epochs):
+			for batch in batches_per_epoch:
+				loss_history.append(func(batch))
+			if epoch % 25 == 0:
+				print "curr loss: %s" % (loss_history[-1])
+				if test_func is not None:
+					print test_func()
+
+
+
+	def get_updates(self, loss, all_params):
+		all_grads = theano.grad(loss, all_params)
+		# all_lrs maps param -> learning rate
+		all_lrs = {param:T.scalar() for param in all_params}
+
+		updates = []
+		for param_i, grad_i in zip(all_params, all_grads):
+			mparam_i = theano.shared(np.zeros(param_i.get_value().shape, dtype=theano.config.floatX),broadcastable=param_i.broadcastable)
+			v = self.momentum * mparam_i - all_lrs[param_i] * grad_i
+			updates.append((mparam_i, v))
+			updates.append((param_i, param_i + v))
+
+		return updates, all_lrs
+
+
+
+
+	def step(self, model, include_bias=True):
+		all_param
+
+
+
+
+
+class BaseSolver(object):
+	def __init__(self, reg='l2', reg_scale=0.01):
+		self.reg = reg
+		self.reg_scale = 0.01
+		self.lasagne_reg = self.find_lasagne_reg()
+
+	def get_reg_loss(self, model, include_bias=False):
+		all_params = self.get_all_params(model, include_bias=include_bias)
+		loss = self.lasagne_reg(all_params)
+		return loss * reg_scale
+
+
+	def find_objective(self, model, objective):
+		if objective == 'multinomial_nll':
+			return lasagne.objectives.Objective(model.last_layer, lasagne.objectives.multinomial_nll)
+
+
+	def find_lasagne_reg(self):
+		if self.reg == 'l2':
+			return lasagne.regularization.l2
+
+	def get_all_params(self, model, include_bias=True):
+		'''
+		finds all the parameters of a lasagne model
+		'''
+		if include_bias:
+			return lasagne.layers.get_all_params(model.last_layer)
+		else:
+			return lasagne.layers.get_all_non_bias_params(model.last_layer)
+
+	def get_updates(self):
+		raise NotImplementedError
