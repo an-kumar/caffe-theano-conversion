0659ae5e78efb66f0a465eae056270090c35a9b7
Ankit Kumar <ankit@Metamind-admins-MacBook-Pro.local>
2015-03-12 12:07:45 -0700

    sending script back

--- MIT-scenes/script.py
+++ MIT-scenes/script.py
@@ -3,5 +3,5 @@ sys.path.insert(0,'../')
 import dataset
 
 ds = dataset.ImageDirectoryDataset('../../proj/Images', 250, 1) 
-ds.dump_to_pickles(mode='train')
+ds.dump_to_pickles(startbatch=11, mode='train')
 ds.dump_to_pickles(mode='test')
